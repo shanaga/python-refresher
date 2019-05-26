3a9627ce2137e92ac98dff9c3edac550a4c3f11b
Shantanu Agarwal <shantanu.agarwal2109@gmail.com>
2019-05-25 22:31:32 -0400

    smallest positive solutions in a list

--- scripts/smallest-positive.py
+++ scripts/smallest-positive.py
@@ -1,21 +1,35 @@
 print('Print smallest positive in list using Solution 1')
 
+
 def smallest_positive_1(in_list):
-    print(in_list)
-    #Sort the list
+    # print(in_list)
+    # Sort the list
     in_list.sort()
-    #print list before finding positive number
-    print(in_list)
-    #smallest_positive = ""
+    # print list before finding positive number
+    # print(in_list)
+    # smallest_positive = ""
     smallest_pos = None
     for num in in_list:
         if num > 0:
             return num
     return smallest_pos
 
-#Call smallest_positive(in_list) to print the lowest number in the list
+
+# Call smallest_positive(in_list) to print the lowest number in the list
 print(smallest_positive_1([4, -6, 7, 2, -4, 10]))
-print(smallest_positive_1([7,3,-9,-1,4]))
+print(smallest_positive_1([7, 3, -9, -1, 4]))
+print(smallest_positive_1([-9, -8, -4, -21, -6]))
 
 
+print('Print smallest positive in list using Solution 2')
+def smallest_positive_2(in_list):
+    #print list before sorting
+    print(in_list)
+    smallest_pos = None
+    for num in in_list:
+        if num > 0:
+            if smallest_pos == None or num < smallest_pos:
+                smallest_pos = num
+    return smallest_pos
 
+print(smallest_positive_2([4, -6, 7, 3, -4, 10]))
